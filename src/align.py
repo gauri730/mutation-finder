ee49a389b5162d6924916a08f7265fc67f0c8495
Gauri Nair <gaanair@ucsc.edu>
2026-07-14 01:59:53 -0700

    Add match, mismatch, gap comparison to matrix fill

--- src/align.py
+++ src/align.py
@@ -6,6 +6,15 @@ def build_matrix(ref, sample, match_score=1, mismatch_pen=-1, gap_pen=-2):
         matrix[k][0] = k * gap_pen
     for l  in range(n + 1):
         matrix[0][l] = l * gap_pen
+    for k in range (1, m+1):
+        for l in range( 1, n+1):
+            if ref[k - 1] == sample[l - 1]:
+                diag = matrix[k - 1][l- 1] + match_score
+            else:
+                diag = matrix[k - 1][l - 1] + mismatch_pen
+            up = matrix[k - 1][l] + gap_pen
+            left = matrix[k][l - 1] + gap_pen
+            matrix[k][l] = max(diag, up, left)
     return matrix
 if __name__ == "__main__":
     test_matrix = build_matrix("GAT", "GCT")
